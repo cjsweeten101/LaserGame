b83f44a8fb3a9c84fc40597867ca3434d646305f
Clayton Sweeten <cjsweeten101@gmail.com>
2018-04-17 21:02:24 -0600

    add gravity and acceleration

--- GameFiles/Player/Player.gd
+++ GameFiles/Player/Player.gd
@@ -1,12 +1,17 @@
 extends KinematicBody2D
 
-export var speed = 500
+export var max_speed = 600
+export var accel = 200
+export var gravity = 10
+var direction = Vector2()
+const UP = Vector2(0, -1)
 
 func _physics_process(delta):
-	var direction = Vector2()
+	direction.y += gravity
 	if Input.is_action_pressed("move_right"):
-		direction.x = 1
-	if Input.is_action_pressed("move_left"):
-		direction.x = -1
-	var normalized_speed = direction.normalized() * speed
-	move_and_slide(normalized_speed)
+		direction.x = min(direction.x + accel, max_speed)
+	elif Input.is_action_pressed("move_left"):
+		direction.x = max(direction.x - accel, -max_speed)
+	else:
+		direction.x = 0
+	direction = move_and_slide(direction)
