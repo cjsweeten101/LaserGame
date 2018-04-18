faf8f61433a183e3b709b0b3c323e240aa9d55f5
Clayton Sweeten <cjsweeten101@gmail.com>
2018-04-17 21:45:36 -0600

    add friction

--- GameFiles/Player/Player.gd
+++ GameFiles/Player/Player.gd
@@ -2,16 +2,28 @@ extends KinematicBody2D
 
 export var max_speed = 600
 export var accel = 200
-export var gravity = 10
+export var gravity = 25
+export var jump_height= -700
 var direction = Vector2()
 const UP = Vector2(0, -1)
 
 func _physics_process(delta):
+	var friction = false
+	
 	direction.y += gravity
 	if Input.is_action_pressed("move_right"):
 		direction.x = min(direction.x + accel, max_speed)
 	elif Input.is_action_pressed("move_left"):
 		direction.x = max(direction.x - accel, -max_speed)
 	else:
-		direction.x = 0
-	direction = move_and_slide(direction)
+		friction = true
+	
+	if is_on_floor():
+		if Input.is_action_pressed("move_up"):
+			direction.y = jump_height
+		if friction == true:
+			direction.x = lerp(direction.x, 0, .4)
+	else:
+		if friction == true:
+			direction.x = lerp(direction.x, 0, .2)
+	direction = move_and_slide(direction, UP)
