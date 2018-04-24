0598db5e5ad695e9c66de25eb11cc6fd724fe950
Clayton Sweeten <cjsweeten101@gmail.com>
2018-04-23 23:37:58 -0600

    started reflections

--- GameFiles/Main.gd
+++ GameFiles/Main.gd
@@ -4,10 +4,14 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 
+signal reflect
+
 var laser_trail = preload("res://Laser/Laser_anim.tscn")
+var raycast = preload("res://RayCast2D.tscn")
 var game_over = false
 
 func _ready():
+	connect('reflect', self, 'reflect_detection')
 	var players = get_tree().get_nodes_in_group("players")
 	for player in players:
 		player.	connect("hit", self, "hit_detection")
@@ -26,7 +30,10 @@ func hit_detection(raycast, player_loc, direction):
 		game_over = true
 	#Mirror collision
 	elif raycast.get_collider().is_in_group("mirrors"):
-		pass
+		var destination_cast = raycast.cast_to.bounce(raycast.get_collision_normal())
+		destination_cast.x *= direction
+		emit_signal("reflect", raycast.get_collision_point(), destination_cast)
+		
 	#trail
 	var t = laser_trail.instance()
 	var d = player_loc - raycast.get_collision_point()
@@ -37,4 +44,27 @@ func hit_detection(raycast, player_loc, direction):
 		t.scale.x = 1
 	elif direction == -1:
 		t.scale.x = -1
+	add_child(t)
+	
+func reflect_detection(starting_point, cast_to_vector):
+	#Todo refactor one day
+	var new_ray = raycast.instance()
+	new_ray.position = starting_point
+	new_ray.cast_to = cast_to_vector
+	add_child(new_ray)
+	
+	print(new_ray.get_collider())
+	
+	var t = laser_trail.instance()
+	var d = starting_point - new_ray.get_collision_point()
+	print(d.length())
+	t.position = starting_point
+	t.region_rect = Rect2(0,0,d.length(),2)
+	var normal_vect = Vector2(1,0)
+	var angle_in_rads = normal_vect.angle_to(new_ray.cast_to)
+	var angle_in_deg = rad2deg(angle_in_rads)
+	print(angle_in_deg)
+	t.rotation_degrees = angle_in_deg
+	t.z_index = -1
+	
 	add_child(t)
---
+++ GameFiles/RayCast2D.gd
@@ -0,0 +1,4 @@
+extends RayCast2D
+
+func _on_Timer_timeout():
+	queue_free()
